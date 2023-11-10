661bb3e7901652f371abb587c11d01a65d6da725
xavmllt <xaviermaillot100@gmail.com>
2023-11-10 19:13:15 +0100

    added regex for the form

---
+++ regex.js
@@ -0,0 +1,30 @@
+// TEST FORMULAIRE EMAIL
+document.getElementById('form');
+form.addEventListener('submit', function(event){
+    // Empêcher le formulaire de se soumettre normalement
+    event.preventDefault();
+
+    // Récupérer les valeurs de l'email et du téléphone depuis les champs de saisie
+    let emailVal = document.getElementById('email').value;
+    let telVal = document.getElementById('tel').value;
+    // NON VALUE FOR THE RESTE IF ITS WRONG
+    let email = document.getElementById("email");
+    let tel = document.getElementById('tel');
+
+    // Expression régulière pour vérifier le format d'un email
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Expression régulière pour vérifier le format d'un numéro de téléphone (10 chiffres)
+    let regexPhone = /^\d{10}$/;
+
+    // Vérifier l'email
+    if(!regexEmail.test(emailVal)){
+        alert("L'email entrée n'est pas valide. Veuillez saisir une adresse email correcte.");
+        email.value = "";
+        return; // Arrêter l'exécution du script si l'email n'est pas valide
+    };
+    // Vérifier le numéro de téléphone
+    if(!regexPhone.test(telVal)){
+        alert("Le numéro de téléphone n'est pas valide. Veuillez saisir un numéro de téléphone correct.");
+        tel.value = ""; 
+    };
+});
--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+// ALERT ON LOGO 4FFUN //
 let logo = document.getElementById('logo');
 logo.addEventListener('click',()=>{
     alert('NE ME TOUCHER PAS 😡 !!');
 });
 
+// ALERT ON BUBLES 4FUN //
 let boules = document.querySelectorAll('.boule__size');
 boules.forEach(function(boule) {
     boule.addEventListener('mouseover', function() {
         alert('❤️');
     });
 });
+
+
+
+
+
 
